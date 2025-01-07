5c4adbd68c52a21d2f962c59b832b5429ac51f37
Triya <triyaroy0401@gmail.com>
2025-01-07 11:01:06 +0530

    altered vite.config.ts for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 5173,
     open: true,
-  }
-})
+  },
+  define: {
+    'process.env.VITE_BACKEND_URL': JSON.stringify(
+      process.env.VITE_BACKEND_URL || 'http://localhost:8000'
+    ),
+  },
+}));
